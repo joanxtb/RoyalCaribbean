1ce9e99617ff0b181bd989902a318ab7003cfecb
Freddy Torres <joanxtb@gmail.com>
2014-03-26 10:23:15 -0430

    Single Page Start
    Single Page Start

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 var app = {        
 
+    /* ROUTING */
+    renderLogin: function () {
+        var __html = 
+            '<div class="main-wrapper">' + 
+                '<div class="header">Royal Caribbean Logo</div>' + 
+                '<div class="login-wrapper">' + 
+                    '<form id="login-form">' + 
+                        '<div class="editor-label">Username</div>' +
+                        '<div class="editor-field"><input type="text" name="username" /></div>' + 
+                        '<div class="editor-label">Password</div>' +
+                        '<div class="editor-field"><input type="password" name="username" /></div>' +
+                        '<div class="editor-field"><input type="submit" value="Log in" /></div>' +
+                    '</form>' +
+                '</div>' +
+            '</div>'
+
+        $('body').html(__html);
+    },
+
+    /* FEEDBACK */
     showAlert: function (message, title) {
         if (navigator.notification)
             navigator.notification.alert(message, null, title, 'OK');
@@ -7,9 +27,11 @@ var app = {
             alert(title ? (title + ': ' + message) : message);
     },
 
+    /* INIT */
     initialize: function() {
         var self = this;
-        self.showAlert('Application initialized successfully', 'Royal Caribbean')
+        //self.showAlert('Application initialized successfully', 'Royal Caribbean')
+        self.renderLogin();        
     }
 };
 
