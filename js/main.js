a6a44f617d071e765e206fe6c30d44f6421a06a4
Freddy Torres <joanxtb@gmail.com>
2014-03-26 11:03:34 -0430

    Getting some style
    some images and css, + templates

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
 var app = {        
 
     /* ROUTING */
-    renderLogin: function () {
-        var __html = 
-            '<div class="main-wrapper">' + 
-                '<div class="header">Royal Caribbean Logo</div>' + 
-                '<div class="login-wrapper">' + 
-                    '<form id="login-form">' + 
-                        '<div class="editor-label">Username</div>' +
-                        '<div class="editor-field"><input type="text" name="username" /></div>' + 
-                        '<div class="editor-label">Password</div>' +
-                        '<div class="editor-field"><input type="password" name="username" /></div>' +
-                        '<div class="editor-field"><input type="submit" value="Log in" /></div>' +
-                    '</form>' +
-                '</div>' +
-            '</div>'
-
-        $('body').html(__html);
+    renderLogin: function () {        
+        $('body').html(this.loginTemplate());
     },
 
     /* FEEDBACK */
@@ -31,6 +17,7 @@ var app = {
     initialize: function() {
         var self = this;
         //self.showAlert('Application initialized successfully', 'Royal Caribbean')
+        this.loginTemplate = Handlebars.compile($("#login-template").html());
         self.renderLogin();        
     }
 };
