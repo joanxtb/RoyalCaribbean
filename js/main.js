abd8ce8904aa255cfdb145a3893c5e53b7cb3c28
Freddy Torres <joanxtb@gmail.com>
2014-03-26 13:16:37 -0430

    UI Enhancement

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var app = {
 
     /* ROUTING */
     renderLogin: function () {        
-        $('#main-wrapper').html(this.loginTemplate());
+        
     },
 
     /* FEEDBACK */
@@ -14,21 +14,41 @@ var app = {
     },
 
     /* INIT */
-    initialize: function() {
+    initialize: function () {        
         var self = this;
+        this.registerEvents();
+        $('#main-wrapper').html(new LoginView().render().el);
         //self.showAlert('Application initialized successfully', 'Royal Caribbean')
-        this.loginTemplate = Handlebars.compile($("#login-template").html());
-        self.renderLogin();        
+        //this.loginTemplate = Handlebars.compile($("#login-template").html());
+        //self.renderLogin();        
+    },
+
+    registerEvents: function () {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function (event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function (event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function (event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function (event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
     }
 };
 
 app.initialize();
 
-/* Click Handlers */
-$(document).on('click', '#btn-login', function (e) {
-    e.preventDefault();
-    showLoader();
-});
+
 
 function showLoader() {
     $.blockUI({
---
+++ js/modules/login.js
@@ -0,0 +1,32 @@
+﻿/* Core */
+var LoginView = function (store) {
+    this.initialize = function () {
+        // Define a div wrapper for the view. The div wrapper is used to attach events.
+        this.el = $('<div/>');
+
+        /* Login button */
+        this.el.on('click', '#btn-login', function (e) {
+            e.preventDefault();
+            $("#login-form").submit();
+        });
+
+        /* Login form */
+        this.el.on('submit', '#login-form', function (e) {
+            e.preventDefault();
+            var __this = $(this);
+            if (__this.valid()) {
+                showLoader();
+            }
+        });
+    };
+
+    this.render = function () {
+        this.el.html(LoginView.template());
+        return this;
+    };
+
+    this.initialize();
+}
+
+/* Templates */
+LoginView.template = Handlebars.compile($("#login-template").html());
